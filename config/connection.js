d86686c08cba2c70155ec4d449036cf7f9b43626
Mike Sasiela <m5455y@gmail.com>
2021-02-20 09:57:02 -0600

    added jaws

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require('mysql');
+var connection;
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
-  password: 'password',
-  database: 'burgers_db',
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else { 
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    // NOTE: Be sure to add your MySQL password here!
+    password: 'password',
+    database: 'burgers_db',
+  });
+}
 
 // Make connection.
 connection.connect((err) => {
@@ -19,4 +24,4 @@ connection.connect((err) => {
 });
 
 // Export connection for our ORM to use.
-module.exports = connection;
+module.exports = connection
